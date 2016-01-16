f3bb7c8ac2d3c4605a243bb9f98465a62ada312c
liufeng <ywf4026@163.com>
2016-01-16 19:22:08 +0800

    jsonp示例工程2

--- src/main/java/com/springapp/mvc/controllers/HelloController.java
+++ src/main/java/com/springapp/mvc/controllers/HelloController.java
@@ -1,5 +1,6 @@
 package com.springapp.mvc.controllers;
 
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,9 +15,17 @@ public class HelloController {
 
 	@RequestMapping(value = "/hello.html")
 	public ModelAndView hello() {
+		String jsoncallback = httpServletRequest.getParameter("callback");
+
 		ModelAndView modelAndView = new ModelAndView();
 		modelAndView.setViewName("hello");
-		modelAndView.addObject("message", "Hello World!");
+
+		if(StringUtils.isEmpty(jsoncallback)){
+			modelAndView.addObject("message", "{\"domain\":\"message from 8081\"}");
+		}else{
+			modelAndView.addObject("message", jsoncallback + "({\"domain\":\"message from 8081\"})");
+
+		}
 
 		return modelAndView;
 	}
