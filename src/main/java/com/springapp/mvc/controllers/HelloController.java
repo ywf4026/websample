29955a8f77279b18efd4d8a86920cd654babc54e
liufeng <ywf4026@163.com>
2016-01-16 16:04:55 +0800

    一个简单的基于spring velocity的web工程

--- src/main/java/com/springapp/mvc/controllers/HelloController.java
+++ src/main/java/com/springapp/mvc/controllers/HelloController.java
@@ -1,19 +1,11 @@
 package com.springapp.mvc.controllers;
 
-import com.springapp.mvc.constants.TradeOrderInfoConstants;
-import com.springapp.mvc.utils.ConvertUtil;
-import com.springapp.mvc.utils.DateUtils;
-import com.springapp.mvc.utils.ReadFileUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
-import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.Map;
 
 @Controller
 public class HelloController {
@@ -26,7 +18,7 @@ public class HelloController {
 
 		ModelAndView modelAndView = new ModelAndView();
 		modelAndView.setViewName("hello");
-		modelAndView.addObject("debug", loc != -1 ? "1" : "0");
+		modelAndView.addObject("message", "Hello World!");
 
 		return modelAndView;
 	}
